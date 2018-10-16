09afec3cb04df08dab9425c46b3b17c976f47027
Adam Barton <adam.barton333@gmail.com>
2018-10-16 01:03:39 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,5 +1,13 @@
 class Superhero 
 
-  attr_accessor :name, :power
+  attr_accessor :name, :power, :bio
+
+  @@superheroes = []
+
+  def initialize(args)
+
+    @@superheroes << self
+    
+  end
 
 end
